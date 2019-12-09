6515654d97117a6dc780009ec924d78e1242d14d
Gregoire Debray-Genty <gregoire.debray-genty@isep.fr>
2019-12-09 16:07:44 +0100

    bug fix on ne peu plus rentrer la page que l'on vux dans l'url et y acceder quand on est pas log

--- index.php
+++ index.php
@@ -4,43 +4,43 @@ session_start();
 require('controller/controller.php');
 
 if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'monProfil') {
+    if ($_GET['action'] == 'monProfil' && isset($_SESSION['rpps'])) {
         monProfil();
     } elseif ($_GET['action'] == 'login') {
         login();
-    } elseif ($_GET['action'] == 'accueil') {
+    } elseif ($_GET['action'] == 'accueil'&& isset($_SESSION['rpps'])) {
         accueil();
-    } elseif ($_GET['action'] == 'passerUnTest') {
+    } elseif ($_GET['action'] == 'passerUnTest'&& isset($_SESSION['rpps'])) {
         passerUnTest();
-    } elseif ($_GET['action'] == 'mesResultats') {
+    } elseif ($_GET['action'] == 'mesResultats'&& isset($_SESSION['rpps'])) {
         mesResultats();
-    } elseif ($_GET['action'] == 'monEvolution') {
+    } elseif ($_GET['action'] == 'monEvolution'&& isset($_SESSION['rpps'])) {
         monEvolution();
-    } elseif ($_GET['action'] == 'resultatsHopital') {
+    } elseif ($_GET['action'] == 'resultatsHopital'&& isset($_SESSION['rpps'])) {
         resultatsHopital();
-    } elseif ($_GET['action'] == 'aide') {
+    } elseif ($_GET['action'] == 'aide'&& isset($_SESSION['rpps'])) {
         aide();
-    } elseif ($_GET['action'] == 'CGU') {
+    } elseif ($_GET['action'] == 'CGU'&& isset($_SESSION['rpps'])) {
         CGU();
-    } elseif ($_GET['action'] == 'planDuSite') {
+    } elseif ($_GET['action'] == 'planDuSite'&& isset($_SESSION['rpps'])) {
         planDuSite();
-    } elseif ($_GET['action'] == 'monProfilModifier') {
+    } elseif ($_GET['action'] == 'monProfilModifier'&& isset($_SESSION['rpps'])) {
         monProfilModifier();
-    } elseif ($_GET['action'] == 'logout') {
+    } elseif ($_GET['action'] == 'logout'&& isset($_SESSION['rpps'])) {
         logout();
-    } elseif ($_GET['action'] == 'accepted') {
+    } elseif ($_GET['action'] == 'accepted'&& isset($_SESSION['rpps'])) {
         accepted();
-    } elseif ($_GET['action'] == 'accueilAdmin') {
+    } elseif ($_GET['action'] == 'accueilAdmin'&& isset($_SESSION['rpps'])) {
         accueilAdmin();
-    } elseif ($_GET['action'] == 'statistiquesAdmin') {
+    } elseif ($_GET['action'] == 'statistiquesAdmin'&& isset($_SESSION['rpps'])) {
         statistiquesAdmin();
-    } elseif ($_GET['action'] == 'gestionUtilisateurs') {
+    } elseif ($_GET['action'] == 'gestionUtilisateurs'&& isset($_SESSION['rpps'])) {
         gestionUtilisateurs();
-    } elseif ($_GET['action'] == 'gestionTests') {
+    } elseif ($_GET['action'] == 'gestionTests'&& isset($_SESSION['rpps'])) {
         gestionTests();
-    } elseif ($_GET['action'] == 'formulaireAdmin') {
+    } elseif ($_GET['action'] == 'formulaireAdmin'&& isset($_SESSION['rpps'])) {
         formulaireAdmin();
-    } elseif ($_GET['action'] == 'envoiMail') {
+    } elseif ($_GET['action'] == 'envoiMail'&& isset($_SESSION['rpps'])) {
         envoiMail();
     } else {
 		login();
