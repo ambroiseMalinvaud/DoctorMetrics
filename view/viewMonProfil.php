8a2593f9cd1892dc3d96677166f6b023ef8738d7
Alexandre Lalau <alexandre.lalau99@gmail.com>
2019-12-20 14:03:52 +0100

    modifiction mon profil

--- view/viewMonProfil.php
+++ view/viewMonProfil.php
@@ -13,26 +13,29 @@
 
 <div id="corps">
 	<div id="infoUser">
-		<h3>Bonjour 
-			<?= $data['firstName'].' '.$data['lastName'];?>
-		</h3>
-		<p><strong>Numéro RPPS</strong><br/>
-			<?= $data['RPPS'];  ?></p>
-		<p><strong>Adresse mail</strong><br/>
-			<?= $data['mail'];  ?></p>
-		<p><strong>Numéro de téléphone</strong><br/>
-			<?= $data['tel'];  ?></p>
-		<div id="profil">
-			<p><strong>Photo de profil<br/></strong></p>
-			<figure>
-				<img id="photoProfil" src="public/Images/PhotoProfil.jpg" alt="Photo de profil">
-			</figure>
-		</div>
-		<div id="infoUserFooter">
+        <div id="ficheprof">
+            <div id="profil">
+                <figure>
+                    <img id="photoProfil" src="public/Images/PhotoProfil.jpg" alt="Photo de profil">
+                </figure>                                                                                              
+            </div>
+            <div id="texte">
+                <p><strong>Bonjour
+                    <?= $data['firstName'].' '.$data['lastName'];?>
+                </strong></p>
+                <p><strong>Numéro RPPS</strong><br/>
+                    <?= $data['RPPS'];  ?></p>
+                <p><strong>Adresse mail</strong><br/>
+                    <?= $data['mail'];  ?></p>
+                <p><strong>Numéro de téléphone</strong><br/>
+                    <?= $data['tel'];  ?></p>
+            </div>
+        </div>
+        <div id="infoUserFooter">
 			<button><strong><a href="index.php?action=monProfilModifier">Modifier</a></strong></button>
 			<button><strong><a href="index.php?action=logout">Se déconnecter</a></strong></button>
 		</div>
-	</div>
+    </div>
 		<?php 
 		if ($data['admin'] == 1 )
 		{
