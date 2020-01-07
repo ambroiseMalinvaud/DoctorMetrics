3cff8adf8e1d1dfe2770526578f4e4fc043f0fd4
Gregoire Debray-Genty <gregoire.debray-genty@isep.fr>
2020-01-07 10:29:34 +0100

    sql

--- doctormetrics.sql
+++ doctormetrics.sql
@@ -81,20 +81,21 @@ CREATE TABLE `users` (
   `path` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `CGU` tinyint(1) DEFAULT '0',
   `admin` tinyint(1) DEFAULT '0',
-  `position` varchar(128) DEFAULT NULL
+  `position` varchar(128) DEFAULT NULL,
+  `avatar` varchar(128) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `users`
 --
 
-INSERT INTO `users` (`RPPS`, `tel`, `mail`, `lastName`, `firstName`, `password`, `path`, `CGU`, `admin`, `position`) VALUES
-('123', '603571876', 'gregoire@debray.com', 'Debray', 'Gregoire', '2acfa5d173e10de2586042ddb6d9d75415148bc2', NULL, 0, 0, NULL),
-('456', '603571877', 'alexandre@perbet.com', 'Perbet', 'Alexandre', '0badd0ef0e968c2cdc5e30c04c69261db825f2cb', NULL, 0, 0, NULL),
-('789', '654546', 'jyrt@sfr.fr', 'Malinvaud', 'Ambroise', '9ec4236a09d01395a838f2e774923b4e8548fd19', NULL, 1, 1, NULL),
-('147', '603571879', 'alexandre@lalau.com', 'Lalau', 'Alexandre', '9009337cf16333f07109b593405cf7552ed8059a', NULL, 0, 0, NULL),
-('258', '55555', 'karim@ouarti.com', 'Ouarti', 'Karim', '82451b41fd7878180b6aa2b54e369cbec4e8032c', NULL, 1, 0, NULL),
-('0', '0545454545', 'jgt@sfr.fr', '5454', '5454', 'fb644351560d8296fe6da332236b1f8d61b2828a', NULL, 0, 0, NULL);
+INSERT INTO `users` (`RPPS`, `tel`, `mail`, `lastName`, `firstName`, `password`, `path`, `CGU`, `admin`, `position`, `avatar`) VALUES
+('123', '603571876', 'gregoire@debray.com', 'Debray', 'Gregoire', '2acfa5d173e10de2586042ddb6d9d75415148bc2', NULL, 0, 0, NULL,NULL),
+('456', '603571877', 'alexandre@perbet.com', 'Perbet', 'Alexandre', '0badd0ef0e968c2cdc5e30c04c69261db825f2cb', NULL, 0, 0, NULL,NULL),
+('789', '654546', 'jyrt@sfr.fr', 'Malinvaud', 'Ambroise', '9ec4236a09d01395a838f2e774923b4e8548fd19', NULL, 1, 1, NULL,NULL),
+('147', '603571879', 'alexandre@lalau.com', 'Lalau', 'Alexandre', '9009337cf16333f07109b593405cf7552ed8059a', NULL, 0, 0, NULL,NULL),
+('258', '55555', 'karim@ouarti.com', 'Ouarti', 'Karim', '82451b41fd7878180b6aa2b54e369cbec4e8032c', NULL, 1, 0, NULL,NULL),
+('0', '0545454545', 'jgt@sfr.fr', '5454', '5454', 'fb644351560d8296fe6da332236b1f8d61b2828a', NULL, 0, 0, NULL,NULL);
 
 --
 -- Index pour les tables exportées
