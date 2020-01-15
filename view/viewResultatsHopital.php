8ea66571933e4e711547aabd10ef37823899c708
Ambroise Malinvaud <ambroise.malinvaud@gmail.com>
2020-01-15 10:58:33 +0100

    Graphs

--- view/viewResultatsHopital.php
+++ view/viewResultatsHopital.php
@@ -7,16 +7,41 @@
 	<link rel="stylesheet" href="public/css/styleHeader.css">
   	<link rel="stylesheet" href="public/css/styleFooter.css">
   	<link rel="icon" type="image/png" href="public/Images/Logo-Doctor-Metrics.png" />
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+
+      google.charts.load('current', {'packages':['corechart']});
+
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Statut');
+        data.addColumn('number', "Nombre d'utilisateurs");
+        data.addRows([
+          ['Apte', <?php echo $number1;?>],
+          ['Inapte', <?php echo $number2;?>]
+        ]);
+
+        var options = {'title':"Pourcentage d'utilisateurs aptes",
+                       'width':400,
+                       'height':300};
+
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
 </head>
-	
+
 <body>
 <div id="mainBlock">
 <?php require('header.html'); ?>
 
     
 <div id="resultatDelHopital">
-    <img class="resultatDelHopital1" src="public/Images/AdministrateursAptent.png">
-    <img class="resultatDelHopital1" src="public/Images/CorpsMedicalApte.png">
+	<div id="chart_div"></div>
 </div>
 </div>
 
